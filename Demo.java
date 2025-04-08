567fd47e49a55f13854ed6117b12b57717bea39a
ananya-gudepu-fk <gudepu.r@flipkart.com>
2025-04-08 12:22:52 +0530

    Some more changes

--- Demo.java
+++ Demo.java
@@ -2,5 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Checking code rabbit");
+        int x =10;
+        System.out.println(x);
     }
 }
